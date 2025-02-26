774c96ef4c3e946d12ce901be01c1c9afffc3eff
Madison Sanchez-Forman <msforman@uw.edu>
2025-02-26 15:32:12 -0800

    tech review ready to be finalized

--- virtual_art_museum/data_aquisition/met_museum.py
+++ virtual_art_museum/data_aquisition/met_museum.py
@@ -1,53 +1,89 @@
 import requests
 import random
-from piece import Piece
 import pandas as pd
 import os
 from tabulate import tabulate
 class MetMuseum:
-    def __init__(self):
-        self.df = pd.read_csv('MetObjects.txt', dtype='str')
+    def __init__(self, file_path):
+        self.df = pd.read_csv(file_path, dtype='str')
         self.fields = self.df.columns
         self.departments = self.get_all_departments()
-        self.table = self.show_fields()
+        self.ids_without_image = []
 
     def show_fields(self):
+        fields_list = list(self.df.columns)
         table = []
-        fields_per_row = 4
-        row = []
-        
-        for i, field in enumerate(self.fields, 1):
-            row.append(field)
-            if i % fields_per_row == 0:
-                table.append(row)
-                row = []
-    
-        # Add any remaining fields
-        if row:
-            while len(row) < fields_per_row:
-                row.append('')
-            table.append(row)
+        for i in range(0, len(fields_list), 6):
+            fields = fields_list[i:i+6]
+            table.append(fields)
+        print(tabulate(table, tablefmt='grid'))
+        return
 
-        table = tabulate(table, tablefmt='grid')
-        return table
     def request_image_url(self, object_id):
-        response = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}")
-        if self.check_status(response):
-            return response.json()['primaryImage']
+        url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if 'primaryImage' in data and data['primaryImage']:
+                return data['primaryImage']
+            else:
+                self.ids_without_image.append(object_id)
+                return False
         else:
-            return None
+            print(f"Error getting {response.url}", response.status_code)
+            return False
+
+    def get_n_random_objects(self, n):
+        objs = self.df.sample(n)
+        data = [self.get_metadata(id) for id in objs['Object ID']]
+        return data
 
     def get_metadata(self, object_id):
-        row = self.df[self.df['Object Number'] == object_id]
-        metadata = {
-            'object_id': row['Object Number'],
-            'title': row['Title'],
-            'artist': row['Artist Display Name'],
-            'department': row['Department'],
-           # 'image_url': self.request_image_url(object_id)
-        }
-        print(metadata)
-        return metadata
+        if object_id not in self.ids_without_image:
+            row = self.df.loc[self.df['Object ID'] == object_id].to_dict(orient='records')
+            url = self.request_image_url(object_id)
+            if url:
+                metadata = {
+                    'object_id': row[0]['Object ID'],
+                    'title': row[0]['Title'],
+                    'artist': row[0]['Artist Display Name'],
+                    'department': row[0]['Department'],
+                    'medium': row[0]['Medium'],
+                    'artist bio': row[0]['Artist Display Bio'],
+                    'region': row[0]['Region'],
+                    'image_url': url
+                }
+                return metadata
+        else: 
+            raise ValueError(f"Object ID {object_id} has no image")
+
+    def get_all_objects_in_department(self, department_id):
+        objs = self.df[self.df['Department'] == department_id]
+        data = [self.get_metadata(obj['Object ID']) for _, obj in objs.iterrows()]
+        return data
+
+    def get_all_departments(self):
+        return self.df['Department'].unique()
+
+
+def main():
+    met = MetMuseum('MetObjects.txt')
+    data = met.get_n_random_objects(3)
+    print(data)
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+    #def get_n_random_objects(self, n):
+    #     objs = self.df.sample(n)
+    #     print(objs)
+    #     data = [self.get_metadata(obj['Object Number']) for _, obj in objs.iterrows()]
+    #     return data
     # def create_piece(self, object_id):
     #     response = requests.get(f"{self.url}objects/{object_id}")
     #     if self.check_status(response):
@@ -60,32 +96,3 @@ class MetMuseum:
     #         pieces = [self.create_piece(id) for id in ids]
     #     else:
     #         return []
-
-    # def get_N_random_objects(self, N):
-    #     pieces = [self.create_piece(id) for id in random.sample(self.all_ids, N) if self.create_piece(id) is not None]
-    #     return pieces
-
-    def get_all_object_ids(self): #probably wont use this -> it will take forever to get all the objects
-        return self.df['Object Number'].tolist()
-
-    def get_all_objects_in_department(self, department_id):
-        return self.df[self.df['Department'] == department_id]
-
-    def get_all_departments(self):
-        return self.df['Department'].unique()
-
-    def check_status(self, response):
-        if response.status_code == 200:
-            return True
-        else:
-            print(f"Error getting {response.url}", response.status_code)
-            return False
-
-def main():
-    met = MetMuseum()
-    print(met.df.head())
-    # print(met.df['Object Number'])
-    print(met.table)
-    print(met.get_metadata('43652'))
-if __name__ == "__main__":
-    main()
