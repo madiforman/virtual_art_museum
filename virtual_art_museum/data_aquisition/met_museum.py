ac4fe7e300b1c45177713a1e889f18966624bf71
Madison Sanchez-Forman <msforman@uw.edu>
2025-02-27 20:41:02 -0800

    continuous integration

--- virtual_art_museum/data_aquisition/met_museum.py
+++ virtual_art_museum/data_aquisition/met_museum.py
@@ -1,44 +1,89 @@
+import os # standard library
 import requests
 import random
-import pandas as pd
-import os
+
+import pandas as pd # third party
 from tabulate import tabulate
+import numpy as np
+
+# if local files needed put here
+
 class MetMuseum:
+    """
+    Initialize the MetMuseum class
+    Params: file_path - str path to the csv file containing the metadata
+    """
+
     def __init__(self, file_path):
-        self.df = pd.read_csv(file_path, dtype='str')
-        self.fields = self.df.columns
-        self.departments = self.get_all_departments()
-        self.ids_without_image = []
+        self.df = pd.read_csv(file_path, dtype='str') # read in csv file
+        self.fields = self.df.columns # get all fields in the dataframe
+        self.departments = self.get_all_departments() # get all departments in the dataframe
 
     def show_fields(self):
+        """
+        Print all fields in the dataframe
+        """
         fields_list = list(self.df.columns)
         table = []
         for i in range(0, len(fields_list), 6):
             fields = fields_list[i:i+6]
             table.append(fields)
         print(tabulate(table, tablefmt='grid'))
-        return
+        return None
 
     def request_image_url(self, object_id):
+        """
+        Request image url for a given object id
+        Params: object_id - str id of the object to request url for
+        Returns: 
+            False - returns False if either the request fails or the object has no image
+            url - link to image if it exists, False otherwise
+        """
         url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
         response = requests.get(url)
-        if response.status_code == 200:
+
+        if response.status_code == 200: # successful request
             data = response.json()
-            if 'primaryImage' in data and data['primaryImage']:
+            if 'primaryImage' in data and data['primaryImage']: # primary image is main display image
                 return data['primaryImage']
             else:
-                self.ids_without_image.append(object_id)
                 return False
         else:
-            print(f"Error getting {response.url}", response.status_code)
+            print(f"Error getting {response.url}", response.status_code) # print response error
             return False
 
+    def _remove_objects_without_image(self): #underscore means private; no need to run this ever again
+        """
+        Remove objects without image from dataframe; will only be ran once at the beginning
+        Once all objects have been removed -> write to new csv file
+        """
+        all_ids = self.df['Object ID'] # get all object ids
+
+        print("Beginning removal...")
+        for id in all_ids:
+            if not self.request_image_url(id): # make request for each object
+                self.df.drop(self.df.loc[self.df['Object ID'] == id].index, inplace=True)
+
+        self.df.to_csv('MetObjects_Final.csv', index=True)
+        print(f"Finished removal. Final length of df: {len(self.df)}")
+        return None
+
     def get_n_random_objects(self, n):
+        """
+        Get n random objects from dataframe
+        Params: n - int number of objects to get
+        Returns: list of metadata for the objects
+        """
         objs = self.df.sample(n)
         data = [self.get_metadata(id) for id in objs['Object ID']]
         return data
 
     def get_metadata(self, object_id):
+        """
+        Get metadata for a given object id
+        Params: object_id - str id of the object to get metadata for
+        Returns: metadata for the object
+        """
         if object_id not in self.ids_without_image:
             row = self.df.loc[self.df['Object ID'] == object_id].to_dict(orient='records')
             url = self.request_image_url(object_id)
@@ -58,6 +103,10 @@ class MetMuseum:
             raise ValueError(f"Object ID {object_id} has no image")
 
     def get_all_objects_in_department(self, department_id):
+        """ 
+        Returns all object ids for objects in a given department
+        Params: 
+        """
         objs = self.df[self.df['Department'] == department_id]
         data = [self.get_metadata(obj['Object ID']) for _, obj in objs.iterrows()]
         return data
@@ -67,9 +116,11 @@ class MetMuseum:
 
 
 def main():
+    print("Starting to remove...")
     met = MetMuseum('MetObjects.txt')
-    data = met.get_n_random_objects(3)
-    print(data)
+    met.remove_objects_without_image()
+    print("Done removing")
+
 if __name__ == "__main__":
     main()
 
--- virtual_art_museum/main.py
+++ virtual_art_museum/main.py
@@ -1,4 +1,3 @@
-import streamlit as st
 import requests
 #images -> primaryImage
 def get_posts():
@@ -16,47 +15,5 @@ def get_posts():
             return None
             
 temp = get_posts()
-st.image(temp['primaryImage'])
 
 
-
-
-# import requests
-
-# # Replace with your actual API key
-# api_key = "etendinfi"
-
-
-# # Basic request to search for "Mona Lisa"
-
-
-# def fetch_artwork(search_term, key):
-#     url = f"https://api.europeana.eu/record/v2/search.json?wskey={key}&query=mona+lisa"
-
-#     params = {
-#         "wskey": key,
-#         "query": search_term,
-#         "qf": "TYPE.IMAGE",
-#         "profile": "rich",
-#         "media": "true"     #returns results with media 
-#     }
-#     try:
-#         response = requests.get(url, params=params)
-#     except requests.RequestsException as e:
-#         st.error(f"Error fetching")
-#         return None
-    
-# def display(artwork):   #for artwork inresults["items"]
-#     pass
-
-# def main():
-#     pass
-
-# if __name__ == "__main__":
-#     main()
-# response = requests.get(url)
-# data = response.json()
-
-# test = data['items']
-# for t in test:
-#     print(f"{t}\n")
